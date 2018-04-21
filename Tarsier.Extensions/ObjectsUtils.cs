c83e9194fc9a5dfb04cc084e2b9a73b336357d62
Tarsier-Marianz <36526792+Tarsier-Marianz@users.noreply.github.com>
2018-04-21 21:43:22 +0800

    +object utility for extensions project

---
+++ Tarsier.Extensions/ObjectsUtils.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tarsier.Extensions {
+     public static class ObjectsUtils {
+        public static string SerializeT<T>(this T objToSerialize) {
+            return JsonConvert.SerializeObject(objToSerialize);
+        }
+
+        public static T DeserializeT<T>(this string toDeserialize) {
+            if(String.IsNullOrEmpty(toDeserialize)) {
+                return default(T); ;
+            }
+            return (T)JsonConvert.DeserializeObject<T>(toDeserialize);
+        }
+    }
+}
