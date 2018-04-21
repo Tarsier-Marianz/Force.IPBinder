2b5af1795c78d0c57b5fd7de7c7dd7548beb7d1b
Tarsier-Marianz <36526792+Tarsier-Marianz@users.noreply.github.com>
2018-04-21 16:32:13 +0800

    +project that handles network adapters infomation

---
+++ Tarsier.Networks/NativeMethods.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Tarsier.Networks {
+    public class NativeMethods {
+        [DllImport("Iphlpapi.dll")]
+        public static extern uint GetAdaptersAddresses(uint Family, uint flags, IntPtr Reserved, IntPtr PAdaptersAddresses, ref uint pOutBufLen);
+
+    }
+}
