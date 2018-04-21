7b4d1c6d9de5786040c650dc68935018de3933cf
Tarsier-Marianz <36526792+Tarsier-Marianz@users.noreply.github.com>
2018-04-21 18:28:38 +0800

    +network locale

---
+++ Force.IPBinder/Enums/NetworkLocale.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Force.IPBinder.Enums {
+    public enum NetworkLocale {
+        None = 0,
+        LocalIP = 1,
+        LocalGUID = 2,
+        Network = 3,
+        Enter = 4
+    }
+}
