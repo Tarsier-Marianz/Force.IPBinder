e7a96213ce1106a21a629fc098fae2f152f1bd4f
Tarsier-Marianz <36526792+Tarsier-Marianz@users.noreply.github.com>
2018-04-24 16:30:04 +0800

    +application helpers for IP validation

---
+++ Force.IPBinder/Helpers/ForceBind.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Force.IPBinder.Helpers {
+    public class ForceBind {
+        private string ForceBindIPx86 = Path.Combine(Application.StartupPath, "ForceBindIP", "ForceBindIP.exe");
+        private string ForceBindIPx64 = Path.Combine(Application.StartupPath, "ForceBindIP", "ForceBindIP64.exe");
+        private string BindIP_DLLx86 = Path.Combine(Application.StartupPath, "ForceBindIP", "BindIP.exe");
+        private string BindIP_DLLx64 = Path.Combine(Application.StartupPath, "ForceBindIP", "BindIP64.exe");
+
+        private string _useForceBindIPFile = string.Empty;
+        public ForceBind() {
+           
+            if (!File.Exists(ForceBindIPx86)) {
+                throw new ArgumentNullException(ForceBindIPx86);
+            }
+            if (!File.Exists(BindIP_DLLx86)) {
+                throw new ArgumentNullException(BindIP_DLLx86);
+            }
+
+            if (!File.Exists(ForceBindIPx64)) {
+                throw new ArgumentNullException(ForceBindIPx64);
+            } if (!File.Exists(BindIP_DLLx64)) {
+                throw new ArgumentNullException(BindIP_DLLx64);
+            }
+
+       }
+
+       public bool StartBind(string filename, string arguments) {
+           try {
+               if (!File.Exists(filename)) {
+                   throw new ArgumentNullException(filename);
+               }
+
+
+           } catch {
+               return false;
+           }
+           return true;
+       }
+    }
+}
---
+++ Force.IPBinder/Helpers/Validator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Tarsier.Extensions;
+
+namespace Force.IPBinder.Helpers {
+    public class Validator {
+        public static bool CheckIPValid(string strIPAddress) {
+            string[] arrOctets = strIPAddress.Split('.');
+            if(arrOctets.Length != 4)
+                return false;
+            foreach(string strOctet in arrOctets) {
+                if(strOctet.Length > 3) //to return false if 0000, 000001
+                    return false;
+                int i = strOctet.ToSafeInteger();
+                if((i < 0) || (i > 255))
+                    return false;
+            }
+            return true;
+        }
+    }
+}
