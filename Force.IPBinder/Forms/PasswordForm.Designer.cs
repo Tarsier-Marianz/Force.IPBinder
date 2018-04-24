af66e6a8a3c0e866881ab75ee3e9f5eeae36557c
Tarsier-Marianz <36526792+Tarsier-Marianz@users.noreply.github.com>
2018-04-24 16:27:09 +0800

    +form for password configuration

---
+++ Force.IPBinder/Forms/PasswordForm.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace Force.IPBinder.Forms {
+    partial class PasswordForm {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if(disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PasswordForm));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtConfirmPassword = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.txtCurrentPassword = new System.Windows.Forms.TextBox();
+            this.lblCurrentPwd = new System.Windows.Forms.Label();
+            this.timerCheck = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "New Password:";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(127, 79);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(222, 21);
+            this.txtPassword.TabIndex = 1;
+            this.txtPassword.UseSystemPasswordChar = true;
+            this.txtPassword.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Password_KeyDown);
+            // 
+            // txtConfirmPassword
+            // 
+            this.txtConfirmPassword.Location = new System.Drawing.Point(127, 115);
+            this.txtConfirmPassword.Name = "txtConfirmPassword";
+            this.txtConfirmPassword.Size = new System.Drawing.Size(222, 21);
+            this.txtConfirmPassword.TabIndex = 2;
+            this.txtConfirmPassword.UseSystemPasswordChar = true;
+            this.txtConfirmPassword.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Password_KeyDown);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Confirm Password:";
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Image = ((System.Drawing.Image)(resources.GetObject("btnCancel.Image")));
+            this.btnCancel.Location = new System.Drawing.Point(274, 165);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 26);
+            this.btnCancel.TabIndex = 4;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // btnSave
+            // 
+            this.btnSave.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnSave.Enabled = false;
+            this.btnSave.Image = ((System.Drawing.Image)(resources.GetObject("btnSave.Image")));
+            this.btnSave.Location = new System.Drawing.Point(193, 165);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 26);
+            this.btnSave.TabIndex = 3;
+            this.btnSave.Text = "Set";
+            this.btnSave.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // txtCurrentPassword
+            // 
+            this.txtCurrentPassword.Location = new System.Drawing.Point(127, 34);
+            this.txtCurrentPassword.Name = "txtCurrentPassword";
+            this.txtCurrentPassword.Size = new System.Drawing.Size(222, 21);
+            this.txtCurrentPassword.TabIndex = 0;
+            this.txtCurrentPassword.UseSystemPasswordChar = true;
+            this.txtCurrentPassword.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Password_KeyDown);
+            // 
+            // lblCurrentPwd
+            // 
+            this.lblCurrentPwd.AutoSize = true;
+            this.lblCurrentPwd.Location = new System.Drawing.Point(24, 37);
+            this.lblCurrentPwd.Name = "lblCurrentPwd";
+            this.lblCurrentPwd.Size = new System.Drawing.Size(97, 13);
+            this.lblCurrentPwd.TabIndex = 10;
+            this.lblCurrentPwd.Text = "Current Password:";
+            // 
+            // timerCheck
+            // 
+            this.timerCheck.Enabled = true;
+            this.timerCheck.Tick += new System.EventHandler(this.timerCheck_Tick);
+            // 
+            // PasswordForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(372, 206);
+            this.Controls.Add(this.txtCurrentPassword);
+            this.Controls.Add(this.lblCurrentPwd);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtConfirmPassword);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Tahoma", 8.25F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "PasswordForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Master Password";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtConfirmPassword;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.TextBox txtCurrentPassword;
+        private System.Windows.Forms.Label lblCurrentPwd;
+        private System.Windows.Forms.Timer timerCheck;
+    }
+}
---
+++ Force.IPBinder/Forms/PasswordForm.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Force.IPBinder.Forms {
+    public partial class PasswordForm : Form {
+        private string _currentPassword = string.Empty;
+        public string Password {
+            get {
+                return txtConfirmPassword.Text.Trim();
+            }
+        }
+        public PasswordForm(string currentPassword) {
+            InitializeComponent();
+            InitPassword(currentPassword);
+        }
+
+        private void InitPassword(string currentPassword) {
+            _currentPassword = currentPassword;
+            if(string.IsNullOrEmpty(_currentPassword)) {
+                lblCurrentPwd.Visible = txtCurrentPassword.Visible = false;
+                txtPassword.Enabled = txtConfirmPassword.Enabled = true;
+            } else {
+                lblCurrentPwd.Visible = txtCurrentPassword.Visible = true;
+                txtPassword.Enabled = txtConfirmPassword.Enabled = false;
+            }
+        }
+
+        private void timerCheck_Tick(object sender, EventArgs e) {
+            if(string.IsNullOrEmpty(_currentPassword)) {
+                btnSave.Enabled = txtPassword.Text.Trim().Length > 2 && txtPassword.Text.Equals(txtConfirmPassword.Text);
+            }else {
+                txtPassword.Enabled = txtCurrentPassword.Text.Equals(_currentPassword);
+                txtConfirmPassword.Enabled = txtPassword.Text.Trim().Length > 2;
+                btnSave.Enabled = _currentPassword.Equals(txtCurrentPassword.Text) && txtPassword.Text.Trim().Length > 2 && txtPassword.Text.Equals(txtConfirmPassword.Text);
+            }
+        }
+
+        private void Password_KeyDown(object sender, KeyEventArgs e) {
+            if(e.KeyCode.Equals(Keys.Enter)) {
+                if(btnSave.Enabled) {
+                    btnSave.PerformClick();
+                }else {
+                    SendKeys.Send("{TAB}");
+                }
+            }
+        }
+    }
+}
