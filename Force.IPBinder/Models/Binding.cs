269450ee379a53bfe33c5d2112db6fd8caece78c
Tarsier-Marianz <36526792+Tarsier-Marianz@users.noreply.github.com>
2018-04-24 16:25:56 +0800

    +models of all controllers

---
+++ Force.IPBinder/Models/Binding.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Force.IPBinder.Models {
+   public class BindingIP {
+        public int ID { get; set; }
+        public string Description { get; set; }
+        public string IPAddress { get; set; }
+        public string Path { get; set; }
+        public int AutoBind { get; set; }
+        public string Code { get; set; }
+    }
+}
---
+++ Force.IPBinder/Models/Config.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Force.IPBinder.Models {
+   public class Config {
+        public bool SetPassword { get; set; }
+        public bool ClearBindList { get; set; }
+    }
+}
