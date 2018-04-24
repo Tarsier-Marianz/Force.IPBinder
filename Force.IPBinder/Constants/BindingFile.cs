d428c5ff3d62fe0188c0f2de45d3ff24104d2034
Tarsier-Marianz <36526792+Tarsier-Marianz@users.noreply.github.com>
2018-04-24 16:24:10 +0800

    +adding controllers handling data to/from database

---
+++ Force.IPBinder/Constants/BindingFile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Force.IPBinder.Constants {
+    public class BindingFile {
+        public const string DatabaseFile = "Tarsier.Bindings.dll";
+    }
+
+    public class Messages {
+        public const string EXIT = "Are you sure you want to exit application?";
+        public const string REMOVE = "{0} network interface will permanently deleted in force binding!\nAre you sure you want to delete selected item in list?";
+        public const string CLEAR = "All binded application to specific network interface will permanently deleted!\nAre you sure you want to delete all items in list?";
+        public const string INVALID_IP = "IP Address {0} is not valid!. Please check your entered IP Address.";
+        public const string EMPTY_IP = "Please provide IP Address to proceed force binding.";
+        public const string EMPTY_EXE = "Please provide executable file of application to proceed force binding.";
+    }
+}
---
+++ Force.IPBinder/Controllers/Configs.cs
@@ -0,0 +1,113 @@
+﻿using Force.IPBinder.Models;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using Tarsier.Config;
+using Tarsier.Extensions;
+
+namespace Force.IPBinder.Controllers {
+    public class Configs {
+        private SQLiteHelper sqlite;
+        private SQLiteTable table;
+        private string defaultTable = "options";
+
+        public string CurrentTable {
+            get { return defaultTable; }
+            set { defaultTable = value; }
+        }
+
+        public Configs(string filename) {
+            sqlite = new SQLiteHelper(filename);
+            CreateTable(defaultTable);
+        }
+        public void SetProfile(string profileName) {
+            defaultTable = profileName.RemoveNonAlphaNumeric().ToLower();
+            CreateTable(defaultTable);
+        }
+
+        public void CreateTable(string tableName) {
+            defaultTable = tableName.RemoveNonAlphaNumeric().ToLower();
+            table = new SQLiteTable(tableName);
+            table.AddColumn(new SQLiteColumn("ID", true));
+            table.AddColumn(new SQLiteColumn("Property", ColType.Text));
+            table.AddColumn(new SQLiteColumn("Value", ColType.Text));
+            sqlite.CreateTable(table);
+        }
+
+        public bool ClearCommands() {
+            try {
+                if(sqlite.IsTableExist(defaultTable)) {
+                    sqlite.DropTable(defaultTable);
+                    return true;
+                }
+            } catch {
+            }
+            return false;
+
+        }
+        public DataTable GetDataTable(bool orderDesc) {
+            if(string.IsNullOrEmpty(defaultTable)) {
+                return null;
+            }
+            if(orderDesc) {
+                return sqlite.Select(string.Format(Queries.SELECT_TABLE_DESC, defaultTable, "ID"));
+            }
+            return sqlite.GetDataTable(defaultTable);
+        }
+
+        public T Get<T>(string property) {
+            T _temp = default(T);
+            try {
+                string dbConfig = GetValue(property);
+                if(!string.IsNullOrEmpty(dbConfig)) {
+                    _temp = dbConfig.DeserializeT<T>();
+                }
+            } catch {
+                return default(T);
+            }
+            return _temp;
+        }
+
+        public string GetValue(string property) {
+            DataTable dt = GetDataTable(false);
+            if(dt != null) {
+                foreach(DataRow r in dt.Rows) {
+                    if(property.Equals(r["Property"].ToSafeString())) {
+                        return r["Value"].ToSafeString();
+                    }
+                }
+            }
+            return string.Empty;
+        }
+
+        public bool Set<T>(string key, T value) {
+            return Add(key, value.SerializeT<T>());
+        }
+        public void Add(Config _cfg) {
+            JObject json = JObject.FromObject(_cfg);
+            foreach(JProperty property in json.Properties()) {
+                Add(property.Name, property.Value.ToSafeString());
+            }
+        }
+
+        public bool Add(string property, string value) {
+            try {
+                Dictionary<string, object> data = new Dictionary<string, object>();
+                //property = property.ToLower();
+                data.Add("Property", property);
+                data.Add("Value", value);
+                if(sqlite.IsExist(defaultTable, "Property", property.ToStringType())) {
+                    sqlite.Update(defaultTable, data, "Property", property);
+                } else {
+                    sqlite.Insert(defaultTable, data);
+                }
+            } catch {
+                return false;
+            }
+            return true;
+        }
+    }
+}
