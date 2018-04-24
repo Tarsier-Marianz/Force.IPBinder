3f8ed70012d035d8cf20ee974495501180880fcf
Tarsier-Marianz <36526792+Tarsier-Marianz@users.noreply.github.com>
2018-04-24 23:11:18 +0800

    +cli parameters

---
+++ Force.IPBinder/Models/CLIParameters.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Force.IPBinder.Helpers {
+    public class CLIParameters {
+        public string Filename { get; set; }
+        public string Arguments { get; set; }
+        public string Prefix { get; set; }
+    }
+}
