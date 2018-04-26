fb2e9ca72e7bff3424fee0bcac5c1e7eb1a5bb8a
Tarsier-Marianz <36526792+Tarsier-Marianz@users.noreply.github.com>
2018-04-26 17:52:28 +0800

    +ping resources

--- Force.IPBinder/Properties/Resources.Designer.cs
+++ Force.IPBinder/Properties/Resources.Designer.cs
@@ -89,5 +89,25 @@ namespace Force.IPBinder.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap ping_start {
+            get {
+                object obj = ResourceManager.GetObject("ping_start", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap ping_stop {
+            get {
+                object obj = ResourceManager.GetObject("ping_stop", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
