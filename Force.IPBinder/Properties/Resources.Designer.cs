acda1243cb649750c63b9416f4d5358984bf3650
Tarsier-Marianz <36526792+Tarsier-Marianz@users.noreply.github.com>
2018-07-25 14:28:44 +0800

    +application resources

--- Force.IPBinder/Properties/Resources.Designer.cs
+++ Force.IPBinder/Properties/Resources.Designer.cs
@@ -109,5 +109,15 @@ namespace Force.IPBinder.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap warning_icon {
+            get {
+                object obj = ResourceManager.GetObject("warning-icon", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
